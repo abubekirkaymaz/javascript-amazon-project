7d156d0735136e2e5504d582e5e8fa0ea3058ff4
Bekir <abubekirkaymaz@gmail.com>
2025-01-23 22:58:22 +0300

    and of the cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -76,7 +76,13 @@ document.querySelectorAll('.js-add-to-cart') //give us a list of all the "add to
         });
       }
       
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      })      
       
-      console.log(cart);
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+      
     })
   }) 
