943a3cc2fbf055752e5790f8aafa3428be965573
Bekir <abubekirkaymaz@gmail.com>
2025-01-26 00:40:45 +0300

    exercise-13-adding

--- data/products.js
+++ data/products.js
@@ -1,5 +1,35 @@
 const products = [
   {
+    id: "id1",
+    image: "images/products/backpack.jpg",
+    name: "extra product-1 backpack",
+    rating: {
+      stars: 4.5,
+      count: 100
+    },
+    priceCents: 5000,
+    keywords: [
+      "deneme",
+      "product",
+      "backpack"
+    ]
+  },
+  {
+    id: "id2",
+    image: "images/products/umbrella.jpg",
+    name: "extra product-2 umbrella",
+    rating: {
+      stars: 5,
+      count: 140
+    },
+    priceCents: 2500,
+    keywords: [
+      "deneme",
+      "product",
+      "umbrella"
+    ]
+  },
+  {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,31 +1,26 @@
 let productsHTML = '';
-
-products.forEach( (product) => {
+products.forEach((product) => {
   productsHTML += `
     <div class="product-container">
       <div class="product-image-container">
         <img class="product-image"
-          src=${product.image}>
+          src="${product.image}">
       </div>
-
       <div class="product-name limit-text-to-2-lines">
         ${product.name}
       </div>
-
       <div class="product-rating-container">
         <img class="product-rating-stars"
-          src="images/ratings/rating-${(product.rating.stars) * 10}.png">
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
           ${product.rating.count}
         </div>
       </div>
-
       <div class="product-price">
-        $${(product.priceCents / 100).toFixed(2)} 
+        $${(product.priceCents / 100).toFixed(2)}
       </div>
-
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -38,51 +33,92 @@ products.forEach( (product) => {
           <option value="10">10</option>
         </select>
       </div>
-
       <div class="product-spacer"></div>
-
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
-
-      <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id= "${product.id}">
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>
-  `
+  `;
 });
-
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelectorAll('.js-add-to-cart') //give us a list of all the "add to cart" button on the page
+document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
+    // This solution uses a feature of JavaScript called a
+    // closure. Each time we run the loop, it will create
+    // a new variable called addedMessageTimeoutId and do
+    // button.addEventListener().
+    // Then, because of closure, the function we give to
+    // button.addEventListener() will get a unique copy
+    // of the addedMessageTimeoutId variable and it will
+    // keep this copy of the variable forever.
+    // (Reminder: closure = if a function has access to a
+    // value/variable, it will always have access to that
+    // value/variable).
+    // This allows us to create many unique copies of the
+    // addedMessageTimeoutId variable (one for every time
+    // we run the loop) so it lets us keep track of many
+    // timeoutIds (one for each product).
+
+    let addedMessageTimeoutId;
+
     button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
-      
+      const {productId} = button.dataset;
+
       let matchingItem;
+
       cart.forEach((item) => {
         if (productId === item.productId) {
           matchingItem = item;
         }
-      })
+      });
+
+      const quantitySelector = document.querySelector(
+        `.js-quantity-selector-${productId}`
+      );
+
+      const quantity = Number(quantitySelector.value);
 
       if (matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += quantity;
       } else {
         cart.push({
-          productId: productId,
-          quantity: 1
+          productId,
+          quantity
         });
       }
-      
+
       let cartQuantity = 0;
 
       cart.forEach((item) => {
         cartQuantity += item.quantity;
-      })      
-      
-      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-      
-    })
-  }) 
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
+      const addedMessage = document.querySelector(
+        `.js-added-to-cart-${productId}`
+      );
+
+      addedMessage.classList.add('added-to-cart-visible');
+
+        // Check if a previous timeoutId exists. If it does,
+        // we will stop it.
+        if (addedMessageTimeoutId) {
+          clearTimeout(addedMessageTimeoutId);
+        }
+
+        const timeoutId = setTimeout(() => {
+          addedMessage.classList.remove('added-to-cart-visible');
+        }, 2000);
+
+        // Save the timeoutId so we can stop it later.
+        addedMessageTimeoutId = timeoutId;
+      });
+  });
